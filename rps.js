cb0f87ce84f3ed74fb9a04b84ba745345f42290f
Marko Galevski <galevski.marko+github@gmail.com>
2023-08-23 18:02:54 +0300

    javascript changes detect button presses

--- rps.js
+++ rps.js
@@ -1,10 +1,28 @@
-
-
 const result = {
 	WIN: 0,
 	LOSS: 1,
 	DRAW: 2
 }
+
+let user_score = 0, computer_score = 0;
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', play_rock);
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', play_paper);
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', play_scissors);
+
+function play_rock(_e) {
+	 one_round('rock')
+}
+function play_paper(_e) {
+	 one_round('paper')
+}
+function play_scissors(_e) {
+	 one_round('scissors')
+}
+
 
 function get_computer_choice() {
 	const rps = ["rock", "paper", "scissors"];
@@ -20,7 +38,7 @@ function beats(computer_choice, win_condition) {
 		}
 }
 
-function is_round_won(user_choice, computer_choice) {
+function evaluate_round(user_choice, computer_choice) {
 	if (user_choice === computer_choice) {
 		return result.DRAW;
 	}
@@ -32,38 +50,38 @@ function is_round_won(user_choice, computer_choice) {
 		case "scissors":
 			return beats(computer_choice, "paper");
 		default:
-			return false;
+			return result.DRAW;
 	}
 	
 }
 
-function one_round() {
-	let user_choice = prompt("Rock, paper, or scissors?").toLowerCase();
+function one_round(user_choice) {
+	console.log(user_choice);
 	let computer_choice = get_computer_choice();
-	return is_round_won(user_choice, computer_choice);
-	
+	switch (evaluate_round(user_choice, computer_choice))
+	{			
+		case result.LOSS: 
+			computer_score++;
+			break;
+		case result.WIN:
+			user_score++;
+			break;
+		default:
+			break;
+	}
 }
+
 function main() {
-	let my_score = 0, computer_score = 0;
 	let i = 0;
-	while (i < 5) {
-		switch (one_round()) {
-			case result.LOSS: 
-				computer_score++;
-				break;
-			case result.WIN:
-				my_score++;
-				break;
-			default:
-				break;
-		}
+	while (i < 1) {
+		one_round();
 		if (my_score === 3 || computer_score === 3) {
 			break;
 		}
 		i++;
 	}
-	let winner = (my_score > computer_score) ? "you" : (my_score === computer_score) ? "nobody" : "the computer";
+	let winner = (user_score > computer_score) ? "you" : (my_score === computer_score) ? "nobody" : "the computer";
 	alert(`game's over! The winner is ${winner}, with a score of ${my_score} : ${computer_score}`);
 }
 
-main()
+// main()
